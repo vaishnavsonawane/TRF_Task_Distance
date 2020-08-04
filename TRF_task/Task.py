d608fc2b9f927a16120b811562f3069f369fedb8
Prithviraj <prithvirajgotepatil@gmail.com>
2020-08-04 15:58:36 +0530

    distance to camera function added

--- TRF_task/Task.py
+++ TRF_task/Task.py
@@ -18,3 +18,25 @@ def find_object(image):
 	c = max(cnts, key = cv2.contourArea)
 
 	return cv2.minAreaRect(c)
+
+def distance_to_camera(objWdth, fclLnth, perWdth):
+	# calculating distance between camera and object
+	return (objWdth * fclLnth) / perWdth
+
+# object pre-defined distance from the camera
+objDist = 12
+
+# object pre-defined width
+objWdth = 6.5
+
+# reading pre-defined image
+image = cv2.imread("images/1.jpeg")
+# finding contour in image
+marker = find_marker(image)
+# manipulating focal length through pre-defined data
+fclLnth = (marker[1][0] * objDist) / objWdth
+
+lst = []
+# accessing images from folder
+for i in range(1, 5):
+	lst.append(str(i) + '.jpeg')
